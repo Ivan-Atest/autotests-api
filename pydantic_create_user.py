8067788c5a4c7c112fdb426abd579d155eb086c2
Ivan-Atest <vodopyanov1192@icloud.com>
2025-11-24 17:33:30 +0300

    Знакомство с PydanticЗнакомство с Pydantic. Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,21 @@
+import uuid
+from pydantic import BaseModel, Field, EmailStr
+
+class UserSchema(BaseModel):
+
+    id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+class CreateUserResponseSchema(BaseModel):
+    user = UserSchema
+
